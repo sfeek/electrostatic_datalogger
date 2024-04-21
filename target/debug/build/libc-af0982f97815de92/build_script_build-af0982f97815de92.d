821c66cf0ba6734810a0ebd43d5eb798330af4c5
Shane Feek <shane.feek@structuretec.local>
2024-04-21 09:28:45 -0400

    Update
    Signed-off-by: Shane Feek <shane.feek@structuretec.local>

---
+++ target/debug/build/libc-af0982f97815de92/build_script_build-af0982f97815de92.d
@@ -0,0 +1,5 @@
+D:\Source Code\electrostatic_datalogger\target\debug\build\libc-af0982f97815de92\build_script_build-af0982f97815de92.exe: C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.98\build.rs
+
+D:\Source Code\electrostatic_datalogger\target\debug\build\libc-af0982f97815de92\build_script_build-af0982f97815de92.d: C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.98\build.rs
+
+C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\libc-0.2.98\build.rs:
---
+++ target/debug/deps/autocfg-0d0844ae4894b912.d
@@ -0,0 +1,9 @@
+D:\Source Code\electrostatic_datalogger\target\debug\deps\autocfg-0d0844ae4894b912.rmeta: C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\lib.rs C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\error.rs C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\version.rs
+
+D:\Source Code\electrostatic_datalogger\target\debug\deps\libautocfg-0d0844ae4894b912.rlib: C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\lib.rs C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\error.rs C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\version.rs
+
+D:\Source Code\electrostatic_datalogger\target\debug\deps\autocfg-0d0844ae4894b912.d: C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\lib.rs C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\error.rs C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\version.rs
+
+C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\lib.rs:
+C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\error.rs:
+C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\autocfg-1.0.1\src\version.rs:
