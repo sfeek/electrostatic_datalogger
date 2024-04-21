e906a312ab469fd27338789435d397efa31bdc82
Shane Feek <shane.feek@structuretec.local>
2024-04-21 18:57:06 -0400

    Small update
    Signed-off-by: Shane Feek <shane.feek@structuretec.local>

--- target/debug/build/num-traits-17712f2f2ee914ee/build_script_build-17712f2f2ee914ee.d
+++
@@ -1,5 +0,0 @@
-D:\Source Code\electrostatic_datalogger\target\debug\build\num-traits-17712f2f2ee914ee\build_script_build-17712f2f2ee914ee.exe: C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\num-traits-0.2.14\build.rs
-
-D:\Source Code\electrostatic_datalogger\target\debug\build\num-traits-17712f2f2ee914ee\build_script_build-17712f2f2ee914ee.d: C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\num-traits-0.2.14\build.rs
-
-C:\Users\shane.feek\.cargo\registry\src\github.com-1ecc6299db9ec823\num-traits-0.2.14\build.rs:
